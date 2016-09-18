55f73cf622586b7351992dcee4450b407baf22ca
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-09-18 23:04:34 +0200

    Add validation to POST method. Get data from form. Generate success/error message that is passed to app.js

--- api/books.php
+++ api/books.php
@@ -7,13 +7,13 @@ include ($dir . '/src/DB.php');
 include ($dir . '/src/Book.php');
 
 $conn = DB::connect();
-header('Content-Type: application/json');
+header('Content-Type: application/json');  // wyłączać do sprawdzania var_dump($_POST), bo cuda wyświetla
 
 
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_GET['id']) && intval($_GET['id']) > 0) {
+    if (isset($_GET['id']) && intval($_GET['id']) > 0) { // to jest id przekazane ajaksem!
         // pobieramy pojedyńczą ksiązkę
         $books = Book::loadFromDB($conn, intval($_GET['id']));
     } else {
@@ -21,14 +21,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $books = Book::loadFromDB($conn);
     }
 
-    echo json_encode($books);
+    echo json_encode($books); // wypluj wynik (do app.js)
+    
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //var_dump($_POST);
+//    var_dump($_POST);
+    // walidacja, chociaz formularz nie przepuści pustych pól
+    if (isset($_POST['title']) &&
+        isset($_POST['author']) &&
+        $_POST['description'] !== '') {
+//        echo 'dane ok'; // żadnych takich czystych tekstów, bo errory będą
+        $title = $_POST['title'];
+        $author = $_POST['author'];
+        $description = $_POST['description'];
+        // odebrać dane ajaxem
+        // stworzyć obiekt nowej książki
+        echo json_encode('Book "' . $title . '" by ' . $author . ' has been added to the library.');
+    } else {
+        echo json_encode('All fields need to be filled.');
+    }
+    
 } elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     parse_str(file_get_contents("php://input"), $put_vars);
-    //var_dump($put_vars);
+    var_dump($put_vars);
+    
 } elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
     parse_str(file_get_contents("php://input"), $del_vars);
-    //var_dump($del_vars);
+    var_dump($del_vars);
 }
 
