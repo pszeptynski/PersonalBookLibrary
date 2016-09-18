911264e7a14a432a89e1981a788f1e6d939a8e5d
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-09-18 18:20:12 +0200

    Add some comments

--- js/app.js
+++ js/app.js
@@ -1,50 +1,37 @@
-// nie działa, poprawić plik, porównać
-
 $(function () {
-    // pobieramy całą listę books  z bazy i dodajemy na stronę
-
+    //pobieramy cala liste books z bazy i dodajemy na strone
     $.ajax({
-        url: 'api/books.php', // bez data, bo chcemy all books
+        url: 'api/books.php', //bez data bo chcemy all books
         type: 'GET',
         dataType: 'json'
     }).done(function (result) {
-//        console.log(result);
         for (var i = 0; i < result.length; i++) {
             var book = JSON.parse(result[i]);
-            var bookDiv = $('<div>').addClass('singleBook').html('<h3 data-id"' + book.title + '</h3>'); // dokonczyc wyswietlanie
+            var bookDiv = $('<div>').addClass('singleBook').html('<h3 data-id="' + book.id + '">' + book.title + '</h3><div class="description"></div>');
             $('#bookList').append(bookDiv);
         }
-
     }).fail(function () {
         console.log('Error');
     });
 
-
-
-
-
-    $('#bookList').on('click', $('singleBook').find('h3'), function () {
-        console.log('klik');
+    //zakładamy event na kliknięcie na tytule, który rozwinie opis ksiązki
+    $('#bookList').on('click', $('.singleBook').find('h3'), function (e) {
+        //jquery events on ajax load elements
         var h3 = $(e.target);
-        var bookID = h3.attr('data-id');
-        console.log(bookID);
+        var bookId = h3.attr('data-id');
 
         $.ajax({
-        url: 'api/books.php', // bez data, bo chcemy all books
-        type: 'GET',
-        data: 'id=' + bookID,
-        dataType: 'json'
-    }).done(function (result) {
-//        console.log(result);
+            url: 'api/books.php',
+            type: 'GET',
+            data: 'id=' + bookId, // tutaj już z data, bo potrzebujemy książkę o konkretnym id
+            dataType: 'json'
+        }).done(function (result) {
+            var book = JSON.parse(result[0]);
+            h3.next('.description').html(book.description);  // wstaw opis książki do diva za elementem h3 z tytułem
+        }).fail(function () {
+            console.log('Error');
+        });
 
-    }).fail(function () {
-        console.log('Error');
+
     });
-
-
-
-    });
-
-
-
 });
