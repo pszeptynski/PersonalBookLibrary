933de226b902ccb72f94cf1b52ab9d87aa0e4f1b
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-09-26 00:39:13 +0200

    Clean up.

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,4 @@ CREATE TABLE `books` (
   `author` varchar(255) COLLATE utf8_polish_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
+)
