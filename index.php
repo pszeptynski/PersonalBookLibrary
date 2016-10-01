30cfcd9aa11ac2e5facc695272343565901204f5
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-10-02 01:34:13 +0200

    Cosmetic changes.

--- index.php
+++ index.php
@@ -7,25 +7,23 @@
         <script type="text/JavaScript" src="./js/app.js"></script>
     </head>
     <body>
-        <!--<form method="POST" action="#" >-->
         <form method="POST" action="api/books.php">
             <fieldset style=" width: 100%">
                 <legend>Add a new book to the library:</legend>
                 <label>Title:</label>
-                <input type="text" name="title" id="title" placeholder="Przygody młota Thora" required><br>
+                <input type="text" name="title" id="title" placeholder="The Catcher in the Rye" required><br>
                 <label>Author:</label>
-                <input type="text" name="author" id="author" placeholder="Odyniec syn Odyna" required><br>
+                <input type="text" name="author" id="author" placeholder="J.D. Salinger" required><br>
                 <label>Description:</label>
-                <!--<input type="text" name="description" id="description" required><br>-->
-                <textarea rows="3" cols="30" name="description" id="description" placeholder="Kolejna część sagi." required></textarea>
+                <textarea rows="4" cols="40" name="description" id="description" placeholder="Depiction of adolescent alienation and loss of innocence in the protagonist Holden Caulfield." required></textarea>
                 <input type="submit" value="Submit" id="submitBtn">
             </fieldset>
         </form>
         <div id="bookAdded">
-            <!-- info czy dodanie książki się udało -->
+            <!-- info about book addition to db -->
         </div>
         <div id="bookList">
-            <!-- tu będą ksiązki z bazy przesłąne AJAXem -->
+            <!-- All books got from db appear here -->
         </div>
 
     </body>
